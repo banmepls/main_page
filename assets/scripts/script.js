59a8f9ec3c575821a27bfd4a477b64511624124e
banmepls <bogdan-svet2003@mail.ru>
2022-09-08 22:11:05 +0300

    Animations

---
+++ assets/scripts/script.js
@@ -0,0 +1,19 @@
+let toggle = true;
+const globalNavDiv = document.getElementById("globalNavDiv");
+const sectionHead = document.getElementsByClassName("section_head")[0];
+const sectionImage = document.getElementsByClassName("section_image")[0];
+
+function globalNavMenuIcon(x) {
+  x.classList.toggle("change");
+  globalNavDiv.style.display = toggle ? 'block' : 'none';
+  toggle = !toggle;
+}
+
+window.onscroll = () => {
+  let pos = window.scrollY;
+  if (window.getComputedStyle(sectionImage).height == "800px") {
+    sectionHead.style.top = `${pos}px`;
+    sectionImage.style.top = `${pos}px`;
+    sectionImage.style.left = `${-pos}px`;
+  }
+}
