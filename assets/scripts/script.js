2c8a5929af6644f14ed3a48f1bb3b37de19e7019
banmepls <bogdan-svet2003@mail.ru>
2022-09-09 11:39:56 +0300

    Smooth

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,15 @@
 let toggle = true;
 const globalNavDiv = document.getElementById("globalNavDiv");
-const sectionHead = document.getElementsByClassName("section_head")[0];
 const sectionImage = document.getElementsByClassName("section_image")[0];
 
 function globalNavMenuIcon(x) {
   x.classList.toggle("change");
-  globalNavDiv.style.display = toggle ? 'block' : 'none';
+  if (toggle) {
+    $(globalNavDiv).animate({height:'1500'}, 600);
+  }
+  else {
+    $(globalNavDiv).animate({height:'0'}, 300);
+  }
   toggle = !toggle;
 }
 
