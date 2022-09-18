782fe9cab15f41528593c2e81f181a52ac206379
banmepls <bogdan-svet2003@mail.ru>
2022-09-18 16:50:57 +0300

    Code Page Added!

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,7 @@
 let toggle = true;
 let toggleImg1 = true;
 let toggleImg2 = true;
+
 const globalNavDiv = document.getElementById("globalNavDiv");
 const globalNavMenu = document.getElementById("globalNavMenuIcon");
 const designHead = document.getElementsByClassName("design_head")[0];
@@ -9,35 +10,41 @@ const sectionImage2 = document.getElementsByClassName("section_image")[1];
 
 function globalNavMenuIcon(x) {
   x.classList.toggle("change");
+
   if (toggle) {
     $(globalNavDiv).animate({height:window.innerHeight}, 600);
   }
   else {
     $(globalNavDiv).animate({height:'0'}, 300);
   }
+
   toggle = !toggle;
 }
 
 function openDesign(Img) {
-  const designImage1 = document.getElementById("design_img1");
-  const designImage2 = document.getElementById("design_img2");
   if (Img == 1) {
+    const designImage1 = document.getElementById("design_img1");
+
     if (toggleImg1) {
       $(designImage1).animate({height:window.innerHeight}, 600);
     }
     else {
       $(designImage1).animate({height:'0'}, 300);
     }
+
     toggleImg1 = !toggleImg1;
   }
 
   if (Img == 2) {
+    const designImage2 = document.getElementById("design_img2");
+
     if (toggleImg2) {
       $(designImage2).animate({height:window.innerHeight}, 600);
     }
     else {
       $(designImage2).animate({height:'0'}, 300);
     }
+
     toggleImg2 = !toggleImg2;
   }
 }
